d193c962bcc1f3a700923506d22a44e1d2575647
geeh.oliveira@hotmail.com <geeh.oliveira@hotmail.com>
2023-09-21 17:53:12 -0300

    uso de metodo

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -2,6 +2,28 @@
 public class MinhaClasse {
 	
 	public static void main (String[] args) {
-		System.out.println("Hello World");
+		/*System.out.println("Hello World");
+		String meuNome = "Gesley";
+		
+		int anoNascimento = 1994;
+		
+		boolean verdadeira = true;
+		
+		System.out.println(meuNome);
+		System.out.println(anoNascimento);*/
+		
+		String primeiroNome = "Gesley";
+		String segundoNome = "Oliveira";
+		
+		String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+		
+		System.out.println(nomeCompleto);
+		
+		
 	}
+	
+	public static String nomeCompleto (String primeiroNome, String segundoNome) {
+		return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+	}
+		
 }
